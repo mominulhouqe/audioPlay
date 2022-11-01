014bf7c254a8e1f38d5dfbca14e3bedeb06e6a4d
Sahinur Islam <infosahinur@gmail.com>
2022-11-01 23:58:55 +0600

    fix

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 var buttons = document.querySelectorAll(".mybutton");
-
-
-for(var i = 0; i<buttons.length; i++){
-
-  buttons[i].addEventListener("click",function(){
-
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-
-
   });
-  
 }
-function makeSound(key){
-  switch(key){
-    case "A" : 
-    var audio = new Audio("audios/a.mp3");
-    audio.play();
-    break;
-  
-    case "B" : 
-    var audio = new Audio("audios/b.mp3");
-    audio.play();
-    break;
-    case "C" : 
-    var audio = new Audio("audios/c.mp3");
-    audio.play();
-    break;
-    case "D" : 
-    var audio = new Audio("audios/d.mp3");
-    audio.play();
-    break;
+
+function makeSound(key) {
+  switch (key) {
+    case "A":
+      var audio = new Audio("audios/a.mp3");
+      audio.play();
+      break;
+    case "B":
+      var audio = new Audio("audios/b.mp3");
+      audio.play();
+      break;
+    case "C":
+      var audio = new Audio("audios/c.mp3");
+      audio.play();
+      break;
+    case "D":
+      var audio = new Audio("audios/d.mp3");
+      audio.play();
+      break;
+    case "E":
+      var audio = new Audio("audios/e.mp3");
+      audio.play();
+      break;
     default:
       console.log(key);
   }
 }
-
